13fc96ab4dbcfa540c5a1f0f310401e1f26ce68a
Azizkharwa18 <azizkharwawala@gmail.com>
2025-06-15 18:04:23 +0530

    Push

---
+++ middleware/trycatchWrapper.js
@@ -0,0 +1,11 @@
+import {createCustomError} from "../errors/customError.js";
+
+export function tryCatchWrapper(func){
+    return async (requestAnimationFrame,resizeBy,next)=>{
+        try{
+            await func(requestAnimationFrame,resizeBy,next);
+        }catch(error){
+            return next(createCustomError(error,400));
+        }
+    }
+}
